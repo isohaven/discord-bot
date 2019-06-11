edb93e11f99334d31bafdf1e24c76385bead607d
isohaven <ridgehaven320@gmail.com>
2019-06-11 19:01:46 -0400

    added kill switch to bot

--- bot.py
+++ bot.py
@@ -6,27 +6,30 @@ settings = json.loads(open('settings.json').read())
 token = settings['token']
 local = False
 if "whitelist" in settings:
-	local = True
-	whitelist = settings['whitelist']
+        local = True
+        whitelist = settings['whitelist']
 
 bot_info = json.loads(open('bot_info.json').read())
 
 class MyClient(discord.Client):
-	async def on_ready(self):
-		print('Logged in as:')
-		print(self.user.name)
-		print(self.user.id)
-		print('------')
-	async def on_message(self, message):
-		if message.author == self.user:
-			return
-		if local:
-			if message.author.id not in whitelist:
-				return
-		# print(f'{message.channel}: {message.author}:{message.author.name}: {message.content}')
+        async def on_ready(self):
+                print('Logged in as:')
+                print(self.user.name)
+                print(self.user.id)
+                print('------')
+        async def on_message(self, message):
+                if message.author == self.user:
+                        return
+                if local:
+                        if message.author.id not in whitelist:
+                                return
+                # print(f'{message.channel}: {message.author}:{message.author.name}: {message.content}')
 
-		if message.content.lower() == 'ping':
-			await message.channel.send('pong')
+                if message.content.lower() == 'ping':
+                        await message.channel.send('pong')
+
+                if message.content.lower() == '!stop':
+                     await client.logout()
 
 
 
